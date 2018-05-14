d32672979e4dbb31e9932b3b6b439a845f0c725b
Mark Allen Evans <president@worldvaporexpo.com>
2018-05-14 20:04:23 +0000

    theming

---
+++ website/etnx.js
@@ -0,0 +1,17 @@
+var api = "//127.0.0.1:8117";
+var coinUnits = 100;
+var poolHost = "pool.electronero.org";
+
+var irc = "irc.freenode.net/#etnx-pools";
+
+var email = "support@support.etnx.pool";
+
+var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
+
+var easyminerDownload = "#";
+
+var blockchainExplorer = "https://blockexplorer.electronero.org/block/{id}";
+
+var transactionExplorer = "https://blockexplorer.electronero.org/tx/{id}";
+
+var themeCss = "themes/default.css";
